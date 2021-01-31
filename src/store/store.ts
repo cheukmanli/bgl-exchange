7451a0f1e5e58e6655227ab4923b3d461d12fd7f
cheukmanli <46696600+cheukmanli@users.noreply.github.com>
2021-01-31 23:34:25 +0800

    configured redux store

---
+++ src/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import ratesReducer from "./reducers/ratesReducer";
+
+const store = configureStore({
+  reducer: { rates: ratesReducer },
+});
+
+export default store;
