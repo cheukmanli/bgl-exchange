8b584dfdea9849ffe11cf3fb98f301bff353efe8
cheukmanli <46696600+cheukmanli@users.noreply.github.com>
2021-01-31 23:56:12 +0800

    implemented view for latest prices

--- src/pages/latestPrice.tsx
+++ src/pages/latestPrice.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -7,6 +8,9 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import { ExchangeRate } from "../store/rates/types";
+import { fetchLatestRates } from "../store/reducers/ratesReducer";
+import { getMovementIcon } from "../utilities/utilities";
 
 const useStyles = makeStyles({
   table: {
@@ -14,53 +18,53 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function LatestPrice() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const currencies: ExchangeRate = useSelector(
+    (state: RootStateOrAny) => state.rates.currencies
+  );
+  const ratesStatus = useSelector(
+    (state: RootStateOrAny) => state.rates.status
+  );
+
+  useEffect(() => {
+    if (ratesStatus === "idle") {
+      dispatch(fetchLatestRates());
+    }
+  }, [ratesStatus, dispatch]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Currency</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {ratesStatus === "succeeded" && (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Currency</TableCell>
+                <TableCell align="center">Price</TableCell>
+                <TableCell align="center">Movement</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.keys(currencies).map((currency) => (
+                <TableRow key={currency}>
+                  <TableCell component="th" scope="row" align="center">
+                    BTC{currency}
+                  </TableCell>
+                  <TableCell component="th" scope="row" align="center">
+                    {currencies[currency].latest.price.toFixed(2)}
+                  </TableCell>
+                  <TableCell scope="row" align="center">
+                    {getMovementIcon(currencies[currency].movement)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   );
 }
