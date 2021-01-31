d70921d0d3883005ebcc4b7861db23b0902c9238
cheukmanli <46696600+cheukmanli@users.noreply.github.com>
2021-02-01 02:13:59 +0800

    clean up code

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import dayjs from "dayjs";
-import React, { useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import { RootStateOrAny, useSelector } from "react-redux";
 import { Price } from "../store/rates/types";
@@ -9,9 +8,7 @@ interface LineGraphProps {
   currency: string;
 }
 
-const API_KEY = "e50eb1e83771163bc12139d19c1bfe6e";
 //const baseURL = "http://api.coinlayer.com/timeframe";
-
 const baseURL = "https://api.exchangerate.host/timeseries";
 
 export default function LineGraph(props: LineGraphProps) {
@@ -27,29 +24,26 @@ export default function LineGraph(props: LineGraphProps) {
     return date;
   };
 
-  const formatDate = (date: Date) => {
-    return dayjs(date).format("YYYY-MM-DD");
+  const formatDate = (timestamp: string) => {
+    return dayjs(timestamp).format("M-D HH:mm");
   };
 
-  const fetchHistorical = async () => {
-    const response = await axios.get(baseURL, {
-      params: {
-        start_date: formatDate(getStartDate()),
-        end_date: formatDate(new Date()),
-        base: "BTC",
-        symbols: props.currency,
-      },
-    });
-    return response.data.rates;
-  };
+  // const fetchHistorical = async () => {
+  //   const response = await axios.get(baseURL, {
+  //     params: {
+  //       start_date: formatDate(getStartDate()),
+  //       end_date: formatDate(new Date()),
+  //       base: "BTC",
+  //       symbols: props.currency,
+  //     },
+  //   });
+  //   return response.data.rates;
+  // };
+
   let data;
   if (props.currency && historicPrices) {
-    //const historicalData = fetchHistorical() as Object;
-
     data = {
-      labels: historicPrices.map((price) =>
-        dayjs(price.timestamp).format("M-D HH:mm")
-      ),
+      labels: historicPrices.map((price) => formatDate(price.timestamp)),
       datasets: [
         {
           label: "BTC" + props.currency,
